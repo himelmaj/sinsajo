e45eed85971cb7aa0e0304986b68ee8dab50bd87
Himel Majumder <himelmajumder1@gmail.com>
2025-02-12 02:10:04 +0100

    refactor: clean up layout component structure for improved readability

--- app/(authenticated)/dashboard/layout.tsx
+++ app/(authenticated)/dashboard/layout.tsx
@@ -1,18 +1,13 @@
-
-
-export default function Layout({ children }: { children: React.ReactNode }) {
+export default function Layout() {
   return (
-
-
-<main className="flex flex-1 min-h-screen p-4 sm:p-8 items-center justify-center ">
-  <div className="flex items-center justify-center gap-2 text-center text-2xl md:text-4xl lg:text-7xl">
-    <span className="text-yellow-600">🚧</span>
-    <span className=" font-[--font-neuebit]">
-      UNDER CONSTRUCTION
-    </span>
-    <span className="text-yellow-600">🚧</span>
-  </div>
-</main>
-
+    <main className="flex flex-1 min-h-screen p-4 sm:p-8 items-center justify-center ">
+      <div className="flex items-center justify-center gap-2 text-center text-2xl md:text-4xl lg:text-7xl">
+        <span className="text-yellow-600">🚧</span>
+        <span className=" font-[--font-neuebit]">
+          UNDER CONSTRUCTION
+        </span>
+        <span className="text-yellow-600">🚧</span>
+      </div>
+    </main>
   )
 }
