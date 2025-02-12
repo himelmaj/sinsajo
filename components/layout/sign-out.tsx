0ce2bd24408e60391fe6633f988b7b40e448a27d
Himel Majumder <himelmajumder1@gmail.com>
2025-02-12 02:51:45 +0100

    refactor: remove unused useSession import from SignOut component

--- components/layout/sign-out.tsx
+++ components/layout/sign-out.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { Button } from "@/components/ui/button"
-import { useSession, signOut } from "@/lib/auth/auth-client";
+import { signOut } from "@/lib/auth/auth-client";
 import { useRouter } from "next/navigation"
 
 const SignOut = () => {
